a678dac0fe55fbeea1eea058c1d6a18b3dbc9e4f
John Platkowski <johnjplatkowski@gmail.com>
2026-01-26 11:50:08 -0600

    minor

---
+++ firmware/src/drivers/BLEDriver.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef BLEDRIVER_H
+#define BLEDRIVER_H
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>   // required for CCCD descriptor
+#include <Arduino.h>
+#include <Wire.h>
+
+
+class BLEDriver
+{
+public:
+
+
+
+private:
+
+
+
+
+};
+
+
+
+
+
+#endif
